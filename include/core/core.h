fd99c4dd64d2d18e46ea6d25f37f66a8cfff9d13
timofei makarov <macintosh1999@mail.ru>
2020-04-14 11:47:43 +0300

    initial commit

---
+++ include/core/core.h
@@ -0,0 +1,8 @@
+//
+// Created by timofei on 14.04.2020.
+//
+
+#ifndef JUNTRACKER_CORE_H
+#define JUNTRACKER_CORE_H
+
+#endif //JUNTRACKER_CORE_H
---
+++ src/templates/core/core.cpp
@@ -0,0 +1,4 @@
+//
+// Created by timofei on 14.04.2020.
+//
+
