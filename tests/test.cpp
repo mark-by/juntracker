dd81c80b0f56c2efcde99306ae7060587c83dae8
thePsina <mishele20@mail.ru>
2020-04-15 17:18:20 +0300

    Add example server. Not work. Try to solve it

---
+++ tests/test.cpp
@@ -0,0 +1,25 @@
+//
+// Created by gg on 15.04.2020.
+//
+
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include "server.hpp"
+
+// Need realisation to do tests
+
+// test connection
+
+// test connection manager
+
+// test server
+
+int main () {
+    try {
+        Server server(boost::asio::ip::tcp::v4(), "7777");
+        server.startServer();
+    } catch (std::exception& err) {
+        std::cerr << err.what() << '\n';
+    }
+}
