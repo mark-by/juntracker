0c671fed41dfdb70589227611310f6d02e1fe156
Andrey Mavrin <andmavrin@yandex.ru>
2020-04-15 18:33:29 +0300

    added SELECT tests

--- project/tests/src/test.cpp
+++ project/tests/src/test.cpp
@@ -66,8 +66,40 @@ TEST(DataBase SelectValues) {
     std::vector<std::string> column_names = {"id", "name", "surname", "age"};
     std::vector<std::variant<std::string, int, double>> values = {2, 'Anna', 'Borisenko', 13};
     for (int col = 0; col < resultSet.get_cols(); col++) {
-        auto column = resultSet.get(col = 1);
+        auto column = resultSet.get(col);
         EXPECT_EQ(column.get_column_name(), column_names[i]);
         EXPECT_EQ(column.get_value(), values[i]);
+    }
+}
+
+TEST(DataBase SelectValues) {
+    const std::string query = "SELECT name, surname FROM teacher, WHERE id = 1;";
+    bool ok = postgres.exec(query, resultSet);
+    EXPECT_EQ(ok, true);
+    std::vector<std::string> column_names = {"name", "surname",};
+    std::vector<std::variant<std::string, int, double>> values = {'Olga', 'Smirnova'};
+
+    for (int col = 0; col < resultSet.get_cols(); col++) {
+        auto column = resultSet.get(col);
+        EXPECT_EQ(column.get_column_name(), column_names[i]);
+        EXPECT_EQ(column.get_value(), values[i]);
+    }
+}
+
+TEST(DataBase SelectValues) {
+    const std::string query = "SELECT name, surname FROM student, WHERE id <= 2;";
+    bool ok = postgres.exec(query, resultSet);
+    EXPECT_EQ(ok, true);
+    std::vector<std::vector<std::variant<std::string, int, double>>> values = {{'Boris', 'Petrov'}, {'Anna', 'Borisenko'}};
+    std::vector<std::string> column_names = {"name", "surname"};
+    int count_rows = 0;
+
+    while (resultSet.next()) {
+        for (int col = 0; col < resultSet.get_cols(); col++) {
+            auto column = resultSet.get(col);
+            EXPECT_EQ(column.get_column_name(), column_names[i]);
+            EXPECT_EQ(column.get_value(), values[count_rows][i]);
+        }
+        count_rows++;
     }
 }
