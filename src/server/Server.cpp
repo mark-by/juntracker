f489608cdd3831ff7d9ce18c621d4837b67d8782
thePsina <mishele20@mail.ru>
2020-05-08 21:58:21 +0300

    threads

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -4,6 +4,8 @@
 
 #include "Server.h"
 #include "boost/bind.hpp"
+#include "boost/thread.hpp"
+#include <thread>
 
 namespace async = boost::asio;
 namespace net   = async::ip;
@@ -15,10 +17,13 @@ Server::Server(const std::string &addr, const std::string &port):
     net::tcp::resolver resolver_(service_);
     net::tcp::resolver::query query_(addr, port);
     net::tcp::endpoint endpoint_ = *resolver_.resolve(query_);
+
     acceptor_.open(endpoint_.protocol());
     acceptor_.set_option(net::tcp::acceptor::reuse_address(true));
+
     acceptor_.bind(endpoint_);
     acceptor_.listen();
+
     acceptor_.async_accept(connection_->socket(),
             boost::bind(&Server::accept,
                     this,
@@ -27,8 +32,18 @@ Server::Server(const std::string &addr, const std::string &port):
 }
 
 void Server::startServer() {
-    // need to think about threads
-    service_.run();
+    std::vector<std::thread> threads;
+
+    for (unsigned int i = 0; i < std::thread::hardware_concurrency(); i++) {
+        threads.push_back(std::thread(boost::bind(
+                &async::io_service::run,
+                &service_)
+                ));
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
 }
 
 void Server::stopServer() {
@@ -46,10 +61,12 @@ void Server::stop() {
 void Server::accept(const boost::system::error_code &error) {
     if (!error) {
         manager_.start(connection_);
+
         connection_.reset(new Connection(
                 service_,
                 manager_
                 ));
+
         acceptor_.async_accept(connection_->socket(),
                 boost::bind(
                         &Server::accept,
@@ -58,8 +75,3 @@ void Server::accept(const boost::system::error_code &error) {
                         ));
     }
 }
-
-
-
-
-
