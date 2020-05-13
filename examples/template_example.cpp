497baa4b1138d14d45365c68467f3c6b375066b2
Mark Bykhovets <mb@hrsmnt.ru>
2020-05-13 08:45:51 +0300

    Написан серьезный шаблон. Работает

--- examples/template_example.cpp
+++ examples/template_example.cpp
@@ -1,10 +1,7 @@
-//#include <loader/loader.h>
 #include <iostream>
 #include <template/template.h>
-#include <parser/re_tags.h>
 
 int main() {
-    std::string filename = "template.html";
     std::string settings_path = "../../test/templates_test/test_files/settings";
     templates::Template templ(settings_path);
     templates::Context context;
@@ -12,6 +9,8 @@ int main() {
     context.put("var", 35585);
     context.putArray("array", std::vector<int>{1,2,3,4});
     context.putArray("jarray", std::vector<int>{55, 66, 77});
+
+    std::string filename = "template.html";
     templ.set(filename);
     std::cout << templ.render(context) << std::endl;
     return 0;
