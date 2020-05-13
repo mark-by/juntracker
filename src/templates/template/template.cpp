9688000241e6ff076ee2f28f2ab74e61150b13cc
Mark Bykhovets <mb@hrsmnt.ru>
2020-05-13 08:50:31 +0300

    Убрана лишняя минимизация

--- src/templates/template/template.cpp
+++ src/templates/template/template.cpp
@@ -4,9 +4,7 @@ std::string templates::Template::render(templates::Context &context) {
     std::string buffer = loader.result();
     auto nodeQueue = parser.parse(buffer.cbegin(), buffer.cend());
     nodeQueue.render(context);
-    std::string result =  nodeQueue.result();
-    templates::Parser::clearString(result);
-    return result;
+    return nodeQueue.result();
 }
 
 void templates::Template::set(const std::string &_filename) {
