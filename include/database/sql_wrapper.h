1231cd0f4a08a8aabf4b0c48027982eb3875f96a
Andrey Mavrin <andmavrin@yandex.ru>
2020-05-14 21:49:25 +0300

    fixed CMakeLists

--- include/database/sql_wrapper.h
+++ include/database/sql_wrapper.h
@@ -10,28 +10,9 @@
 #include <variant>
 #include <vector>
 
-class DBConnection {
-    virtual std::shared_ptr<PGconn> connection() const = 0;
-};
-
-class PGConnection : DBConnection {
-public:
-    PGConnection();
-    std::shared_ptr<PGconn> connection() const override;
-
-private:
-    std::string m_dbhost = "localhost";
-    int         m_dbport = 5432;
-    std::string m_dbname = "jun_tracker";
-    std::string m_dbuser = "amavrin";
-    std::string m_dbpass = "root";
-    std::shared_ptr<PGconn>  m_connection;
-};
-
 class SqlWrapper {
 private:
     PGconn *conn;
-    std::shared_ptr<PGConnection> m_connection;
 
 public:
     explicit SqlWrapper() {}
