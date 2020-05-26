9bc2ca7b01b3ffcf8a24199461f4964ac2867fa5
Mark Bykhovets <mb@hrsmnt.ru>
2020-05-27 01:36:22 +0300

    response and start userapi

---
+++ include/core/UserAPI.h
@@ -0,0 +1,13 @@
+#ifndef JUNTRACKER_USERAPI_H
+#define JUNTRACKER_USERAPI_H
+#include <string>
+#include <unordered_map>
+
+class UserAPI {
+public:
+    UserAPI() = default;
+
+    std::string signUp(const std::unordered_map<std::string, std::string> &data);
+    std::string signIn(const std::string& username, const std::string& password);
+};
+#endif //JUNTRACKER_USERAPI_H
