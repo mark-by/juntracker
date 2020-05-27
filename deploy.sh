5e5dd06a19717dff0df89cc463bdf9b69ce7efa9
Mark Bykhovets <mb@hrsmnt.ru>
2020-05-27 03:54:55 +0000

    Deploy settings

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+cd build
+cmake --build . --target server -- -j 4
+cd ..
+mv build/examples/server bin/.
+rm -rf bin/templates
+rm -rf bin/includes
+rm -rf bin/static
+cp -r test/templates_test/templatesJuntracker/templates bin/.
+cp -r test/templates_test/templatesJuntracker/includes bin/.
+cp -r test/templates_test/templatesJuntracker/static bin/.
+sudo service supervisor restart
