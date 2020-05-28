ec692bf485e7e0515827e2610cbf6909791a97bd
Mark Bykhovets <mb@hrsmnt.ru>
2020-05-28 15:52:57 +0300

    remove debug cout

--- src/wrapper/user.cpp
+++ src/wrapper/user.cpp
@@ -8,8 +8,6 @@ std::vector<Lesson> User::get_current_lessons() const {
     std::string query = "SELECT * FROM lesson WHERE weekday='" + std::to_string(curr_weekday) + "';";
     PGresult *result = nullptr;
     if (!_postgres.query(query, &result)) {
-        std::cout << query << std::endl;
-        std::cout << "CURRENT LESSONS NOT FOUND" << std::endl;
         throw std::exception();
     }
     std::vector<Lesson> res_lesson;
@@ -21,7 +19,6 @@ std::vector<Lesson> User::get_current_lessons() const {
         auto curr_lesson = Lesson(l_id, l_cabinet, curr_weekday, l_start_time, l_end_time, postgres);
         res_lesson.push_back(curr_lesson);
     }
-    std::cout << "COUNT ELSSONS: " << res_lesson.size() << std::endl;
     return res_lesson;
 }
 
@@ -30,8 +27,6 @@ std::vector<Lesson> User::get_lessons_by_weekday(int l_weekday) const {
     std::string query = "SELECT * FROM lesson WHERE weekday='" + std::to_string(l_weekday) + "';";
     PGresult *result = nullptr;
     if (!_postgres.query(query, &result)) {
-        std::cout << query << std::endl;
-        std::cout << "LESSONS BY WEEKDAY " << l_weekday << "NOT FOUND" << std::endl;
         throw std::exception();
     }
     std::vector<Lesson> res_lesson;
@@ -43,7 +38,6 @@ std::vector<Lesson> User::get_lessons_by_weekday(int l_weekday) const {
         auto curr_lesson = Lesson(l_id, l_cabinet, l_weekday, l_start_time, l_end_time, postgres);
         res_lesson.push_back(curr_lesson);
     }
-    std::cout << "COUNT ELSSONS FOR "<< l_weekday<< " WEEKDAY: " << res_lesson.size() << std::endl;
     return res_lesson;
 }
 
@@ -87,20 +81,16 @@ int User::save(const std::string &username, const std::string &password, const s
     auto postgres = connect();
 
     if(!postgres.is_connected()) {
-        std::cout << "conn fail: " << PQerrorMessage(postgres.getConn()) << std::endl;
         return 1;
     }
-    std::cout << "DATABASE CONECTED" << std::endl;
 
     std::ostringstream s;
     std::string table_name = "users";
     int count_rows = postgres.count_rows(table_name);
-    std::cout << count_rows + 1 << std::endl;
     s << "INSERT INTO users VALUES (" << std::to_string(count_rows + 1) << ", '" << email
       << "', '" << username << "', '" << password << "');";
 
     std::string query = s.str();
-    std::cout << query << std::endl;
     if (!postgres.exec(query)) {
         return -1;
     }
