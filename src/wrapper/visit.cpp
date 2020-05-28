a418a2fc1f1f322bd081cdeb1f8444301cf6da42
Mark Bykhovets <mb@hrsmnt.ru>
2020-05-28 19:35:33 +0300

    debug

--- src/wrapper/visit.cpp
+++ src/wrapper/visit.cpp
@@ -61,8 +61,8 @@ int Visit::save(int student_id, int lesson_id, bool was_in_class) {
     std::ostringstream s;
     std::string table_name = "visit";
     int count_rows = postgres.count_rows(table_name);
-    s << "INSERT INTO visit VALUES (" << std::to_string(count_rows + 1) << ", '"
-      << student_id << ", " << lesson_id << ", "
+    s << "INSERT INTO visit VALUES (" << std::to_string(count_rows + 1) << ", "
+      << student_id << ", " << lesson_id << ", '"
       << (was_in_class ? 't' : 'f')  << "');";
 
     std::string query = s.str();
