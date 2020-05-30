312db9736187f1ffb423c9abb384669f5855bb7c
thePsina <mishele20@mail.ru>
2020-05-30 20:48:13 +0300

    user.save

--- include/database/user.h
+++ include/database/user.h
@@ -12,9 +12,14 @@ enum Permission {
 class User {
 public:
     explicit User(int _id, const std::string &_email, const std::string &_login, int _school_id,
-                  const std::string &_password, int _permission, const std::string &_avatar)
-            : _id(_id), _email(_email), _login(_login), _school_id(_school_id), _password(_password),
-              _permission(_permission), _avatar(_avatar) {}
+                  const std::string &_password, int _permission, const std::string &_avatar):
+                  _id(_id),
+                  _email(_email),
+                  _login(_login),
+                  _password(_password),
+                  _permission(_permission),
+                  _avatar(_avatar),
+                  _school_id(_school_id) { }
 
     std::vector<Lesson> get_current_lessons() const;
     std::vector<Lesson> get_lessons_by_weekday(int l_weekday) const;
@@ -22,7 +27,8 @@ public:
 
     static User get_user(int user_id);
     static User get_user(const std::string &username);
-    static int save(const std::string &username, const std::string &password, const std::string &email, int permission);
+    static int save(const std::string &username, const std::string &password,
+            const std::string &email, int permission, int school_id);
     static int remove(int user_id);
 
     int id() const { return _id; }
@@ -40,10 +46,10 @@ private:
     int _id;
     std::string _email;
     std::string _login;
-    int _school_id;
     std::string _password;
     int _permission;
     std::string _avatar;
+    int _school_id;
 
 };
 
