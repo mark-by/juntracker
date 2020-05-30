9a08161f437a402ff7559c9637c8ec7997f9e827
Mark Bykhovets <mb@hrsmnt.ru>
2020-05-30 19:33:34 +0300

    debug

--- include/core/API.h
+++ include/core/API.h
@@ -1,18 +1,36 @@
-//
-// Created by timofei on 22.04.2020.
-//
-
 #ifndef CORE_API_H
 #define CORE_API_H
 
 #include <string>
-#include <../database/student.h>
-#include <../database/lesson.h>
-#include "../templates/context/context.h"
+#include <student.h>
+#include <lesson.h>
+#include <context/context.h>
+#include <teacher.h>
 
 class API {
 public:
     virtual std::string getMainPage(int user_id) = 0;
+
+protected:
+    template<class Person>
+    struct SimplePersonSerializer {
+        templates::Context operator()(const Person &person) {
+            templates::Context context;
+            context.put("name", person.name() + person.surname());
+            context.put("id", person.id());
+            return context;
+        }
+    };
+
+    template<class Title>
+    struct SimpleTitleSerializer {
+        templates::Context operator()(const Title &title) {
+            templates::Context context;
+            context.put("title", title.title());
+            context.put("id", title.id());
+            return context;
+        }
+    };
 };
 
 
