e2617e0dbeba92aaacd1917a552a6c9da0ae4c4f
thePsina <mishele20@mail.ru>
2020-05-31 01:43:55 +0300

    debug

--- src/wrapper/user.cpp
+++ src/wrapper/user.cpp
@@ -160,7 +160,7 @@ User User::get_user(const std::string &username) {
 
 std::vector<Cabinet> User::get_cabinets() const {
     SqlWrapper db;
-    db << "select * from teacher join users on teacher.user_id=users.id where users.school_id=" << _school_id << ";";
+    db << "select * from cabinet school_id=" << _school_id << ";";
     db.exec("Get cabinets");
 
     std::vector<Cabinet> cabinets;
