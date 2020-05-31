06fc1eb0f52a7287fe4dc4a5e4d15e9dd2f21fed
timofei makarov <macintosh1999@mail.ru>
2020-05-31 18:47:55 +0300

    corrected mark

--- src/wrapper/mark.cpp
+++ src/wrapper/mark.cpp
@@ -6,7 +6,9 @@
 
 void Mark::save(int student_id, int course_id, int teacher_id, int school_id, int mark, boost::posix_time::ptime mark_date) {
     SqlWrapper db;
-    std::string date = DateTimeConverter.convert(mark_date);
+    const std::string format = "%Y-%m-%d";
+    DateTimeConverter converter(format);
+    std::string date = converter.convert(mark_date);
 
     db << "insert into mark (student_id, course_id, teacher_id, school_id, mark, mark_date) values("
        << student_id << ", " << course_id << ", " << teacher_id << ", '"
@@ -18,7 +20,9 @@ void Mark::save(int student_id, int course_id, int teacher_id, int school_id, in
 
 void Mark::update(int id, int student_id, int course_id, int teacher_id, int school_id, int mark, boost::posix_time::ptime mark_date) {
     SqlWrapper db;
-    std::string date = DateTimeConverter.convert(mark_date);
+    const std::string format = "%Y-%m-%d";
+    DateTimeConverter converter(format);
+    std::string date = converter.convert(mark_date);
 
     db << "update mark set"
         << " student_id=" << student_id
