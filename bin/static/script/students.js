fa2093e2fffd12b3f962b6cb21b496e1255fefed
Mark Bykhovets <mb@hrsmnt.ru>
2020-06-03 01:31:32 +0300

    нельзя изменять курс у урока

---
+++ bin/static/script/students.js
@@ -0,0 +1,10 @@
+import Component from "./Component.js";
+import Root from "./root.js";
+
+class SearchList extends Component {
+    constructor() {
+        super();
+    }
+}
+
+const root = new Root()
