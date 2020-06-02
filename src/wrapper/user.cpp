43e15824a2a3b507602723417597175748abbf83
thePsina <mishele20@mail.ru>
2020-06-02 17:30:29 +0300

    remove debug

--- src/wrapper/user.cpp
+++ src/wrapper/user.cpp
@@ -29,7 +29,7 @@ std::vector<Lesson> User::get_current_lessons() const {
     SqlWrapper db;
     boost::gregorian::date d = boost::gregorian::day_clock::universal_day();
     int curr_weekday = d.day_of_week().as_number() - 1;
-    db << "SELECT * FROM lesson WHERE weekday='" << curr_weekday << "' and school_id=" << _school_id << ";";
+    db << "SELECT * FROM lesson WHERE weekday=" << curr_weekday << " and school_id=" << _school_id << ";";
     db.exec("Get lessons for weekday by user");
 
     std::vector<Lesson> res_lesson;
@@ -53,7 +53,7 @@ std::vector<Lesson> User::get_current_lessons() const {
 std::vector<Lesson> User::get_lessons_by_weekday(int l_weekday) const {
     SqlWrapper db;
 
-    db << "SELECT * FROM lesson WHERE weekday='" << l_weekday << "' and school_id=" << _school_id << ";";
+    db << "SELECT * FROM lesson WHERE weekday=" << l_weekday << " and school_id=" << _school_id << ";";
     db.exec("Get lessons for weekday by user");
     std::vector<Lesson> res_lesson;
     res_lesson.reserve(db.count_tupls());
@@ -147,7 +147,7 @@ int User::remove(int user_id) {
 
 User User::get_user(const std::string &username) {
     SqlWrapper db;
-    db << "SELECT * FROM users WHERE login=" << username << ";";
+    db << "SELECT * FROM users WHERE login='" << username << "';";
     db.exec("Get user by username");
 
     db.disconnect();
