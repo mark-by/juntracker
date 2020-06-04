9bd6e50963eadcd9c530749697d34eecbcc69657
timofei makarov <macintosh1999@mail.ru>
2020-06-04 13:32:18 +0300

    formatted AdminAPI

--- include/core/AdminAPI.h
+++ include/core/AdminAPI.h
@@ -13,45 +13,39 @@ class AdminAPI : public StaffAPI {
 public:
     AdminAPI() = default;
 
-    int updateLesson(const std::unordered_multimap<std::string, std::string> & data, const User & user);
-
-    static int saveCurrentLesson(const std::unordered_multimap<std::string, std::string> &data);
-
-    int deleteStudents(const std::unordered_multimap<std::string, std::string> &students, const User& user);
-
-    std::string getPageStudents(const User& user);
-
-    int editCourse(const std::unordered_multimap<std::string, std::string> &data, const User &user);
-
-    int deleteCourse(int course_id);
-
-    int createLesson(const std::unordered_multimap<std::string, std::string> &data, const User &user);
-
-    int deleteLesson(const int lesson_id);
-
-    std::string getPagePaymentsByStudent(const std::string &);
-
-    std::pair<int, templates::Context> saveStudent(const std::unordered_multimap<std::string, std::string> &data, const User &user);
-
     templates::Template _render;
 
-    templates::Context searchStudent(const std::string &search, const User& user);
-
     std::string getMainPage(const User & user) override;
+    std::string getPageStudents(const User& user);
+    std::string getStudentPage(int student_id, const User& user);
+
+    static int createLesson(const std::unordered_multimap<std::string, std::string> &data, const User &user);
+    static int updateLesson(const std::unordered_multimap<std::string, std::string> & data, const User & user);
+    static int deleteLesson(int lesson_id);
+    static int saveCurrentLesson(const std::unordered_multimap<std::string, std::string> &data);
+
+    static std::pair<int, templates::Context>
+    saveStudent(const std::unordered_multimap<std::string, std::string> &data, const User &user);
+    static int deleteStudents(const std::unordered_multimap<std::string, std::string> &students, const User& user);
+    static templates::Context searchStudent(const std::string &search, const User& user);
+    static templates::Context verboseSearchStudent(const std::string &search, const User &user);
+
+    static int editCourse(const std::unordered_multimap<std::string, std::string> &data, const User &user);
+    static int deleteCourse(int course_id);
+
+    static int editTeacher(const std::unordered_multimap<std::string, std::string> &data, const User &user);
+    static int deleteTeacher(int  teacher_id);
+
+    static int editCabinet(const std::unordered_multimap<std::string, std::string> &data, const User &user);
+    static int deleteCabinet(int cabinet_id);
 
     std::string schedule(const User &user);
 
-    int editTeacher(const std::unordered_multimap<std::string, std::string> &data, const User &user);
-
-    int deleteTeacher(int  teacher_id);
-
-    int editCabinet(const std::unordered_multimap<std::string, std::string> &data, const User &user);
-
-    int deleteCabinet(int cabinet_id);
-
-    std::string getStudentPage(int student_id, const User& user);
-
-    templates::Context verboseSearchStudent(const std::string &search, const User &user);
+    std::string getPagePaymentsByStudent(const std::string &);
+private:
+    static templates::Context StudentDBSerializer(const Student &student);
+    static templates::Context VisitSerializer(const Visit& visit);
+    static templates::Context StudentVerboseSerializer(const Student& student);
 };
 
 #endif //CORE_ADMINAPI_H
--- src/core/AdminAPI.cpp
+++ src/core/AdminAPI.cpp
@@ -7,44 +7,14 @@
 #include <utils.hpp>
 
 
-int AdminAPI::saveCurrentLesson(const std::unordered_multimap<std::string, std::string> &data) {
-    if (data.empty()) {
-        return 400;
-    }
+std::string AdminAPI::getMainPage(const User &user) {
+    auto context = mainStaffData(user);
+    context.putArray("teachers", user.get_teachers(), PersonSerializer<Teacher>());
+    context.putArray("courses", user.get_courses(), CourseSerializer);
+    context.putArray("cabinets", user.get_cabinets(), SimpleTitleSerializer<Cabinet>());
+    _render.set("mainPageStaff.html");
 
-    int lesson_id = std::stoi(get("lesson_id", data));
-
-    for (auto &pair : data) {
-        if (pair.first != "check" && pair.first != "lesson_id") {
-            Visit::save(std::stoi(pair.first), lesson_id, pair.second[0] == '1');
-        }
-    }
-
-    return 200;
-}
-
-int AdminAPI::deleteStudents(const std::unordered_multimap<std::string, std::string> &students, const User &user) {
-    for (auto &pair : students) {
-        if (pair.first == "id")
-            Student::remove(std::stoi(pair.second));
-    }
-
-    return 200;
-}
-
-templates::Context StudentDBSerializer(const Student &student) {
-    templates::Context context;
-    context.put("name", student.name() + " " + student.surname());
-    context.put("avatar", "");
-    context.put("id", student.id());
-    context.put("age", student.age());
-    std::vector<std::string> courses;
-    for (auto &course : student.get_courses()) {
-        courses.push_back(course.title());
-    }
-    context.putArray("courses", courses);
-
-    return context;
+    return _render.render(context);
 }
 
 std::string AdminAPI::getPageStudents(const User &user) {
@@ -56,136 +26,12 @@ std::string AdminAPI::getPageStudents(const User &user) {
     return _render.render(context);
 }
 
-int AdminAPI::editCourse(const std::unordered_multimap<std::string, std::string> &data, const User &user) {
-    if (data.empty()) {
-        return 404;
-    }
-
-    std::string result;
-    bool success;
-    std::tie(result, success) = fetch("title", data);
-    if (!success)
-        return 404;
-    auto name = result;
-
-    std::tie(result, success) = fetch("price", data);
-    if (!success)
-        return 404;
-    int price = std::stoi(result);
-
-    std::tie(result, success) = fetch("id", data);
-    if (!success)
-        return 404;
-    int id = std::stoi(result);
-
-    if (id == -1) {
-        Course::save(name, price, user.school_id());
-    } else {
-        Course::update(id, name, price, user.school_id());
-    }
-
-    return 200;
-}
-
-int AdminAPI::deleteCourse(int courseId) {
-    try {
-        Course::remove(courseId);
-        return 200;
-    } catch(...) {
-        return 400;
-    }
-}
-
-std::string AdminAPI::getPagePaymentsByStudent(const std::string &) {
-    return std::string();
-}
-
-int AdminAPI::updateLesson(const std::unordered_multimap<std::string, std::string> &data, const User &user) {
-    if (data.empty()) {
-        return 404;
-    }
-
-    int lesson_id = std::stoi(get("lesson_id", data));
-    int course_id = std::stoi(get("course", data));
-    std::vector<int> students_id;
-    for (auto &pair : data) {
-        if (pair.first == "new_student") {
-            Lesson::add_student(std::stoi(pair.second), lesson_id, course_id);
-        } else if (pair.first == "delete_student") {
-            Lesson::delete_student(std::stoi(pair.second), lesson_id, course_id);
-        }
-    }
-
-    auto lesson = Lesson::get_lesson(lesson_id);
-    lesson.update(lesson_id,
-                  std::stoi(get("course", data)),
-                  std::stoi(get("cabinet", data)),
-                  std::stoi(get("teacher", data)),
-                  std::stoi(get("weekday", data)),
-                  get("start-hours", data) + ":" + get("start-minutes", data),
-                  get("end-hours", data) + ":" + get("end-minutes", data),
-                  lesson.school_id());
-    auto students = lesson.get_students();
-
-    return 200;
-}
-
-std::pair<int, templates::Context>
-AdminAPI::saveStudent(const std::unordered_multimap<std::string, std::string> &student, const User &user) {
+std::string AdminAPI::getStudentPage(int student_id, const User& user) {
     templates::Context context;
-
-    if (student.empty()) {
-        return {404, context};
-    }
-
-    std::string result;
-    bool success;
-    std::tie(result, success) = fetch("name", student);
-    if (!success)
-        return {404, context};
-    auto name = result;
-    std::tie(result, success) = fetch("surname", student);
-    if (!success)
-        return {404, context};
-    auto surname = result;
-    auto res = get("age", student);
-    int age = res.empty() ? 0 : std::stoi(res);
-    auto description = get("description", student);
-    auto tel_number = get("tel_number", student);
-    auto email = get("email", student);
-    auto parent = get("parent", student);
-    auto avatar = get("avatar", student);
-
-    int school_id = user.school_id();
-
-    int id = Student::save(name, surname, age, description, tel_number, email, parent, avatar, school_id);
-    context.put("id", id);
-
-    return {200, context};
-}
-
-templates::Context AdminAPI::searchStudent(const std::string &search, const User &user) {
-    templates::Context context;
-    auto students = Student::get_students_like(search, user.school_id());
-    context.putArray("students", students, SimplePersonSerializer<Student>());
-
-    return context;
-}
-
-templates::Context AdminAPI::verboseSearchStudent(const std::string &search, const User &user) {
-    templates::Context context;
-    auto students = Student::get_students_like(search, user.school_id());
-    context.putArray("students", students, StudentDBSerializer);
-    return context;
-}
-
-std::string AdminAPI::getMainPage(const User &user) {
-    auto context = mainStaffData(user);
-    context.putArray("teachers", user.get_teachers(), PersonSerializer<Teacher>());
-    context.putArray("courses", user.get_courses(), CourseSerializer);
-    context.putArray("cabinets", user.get_cabinets(), SimpleTitleSerializer<Cabinet>());
-    _render.set("mainPageStaff.html");
-
+    auto student = Student::get_student(student_id);
+    context.set("student", StudentVerboseSerializer(student));
+    context.set("user", UserSerializer(user));
+    _render.set("student.html");
     return _render.render(context);
 }
 
@@ -229,18 +75,155 @@ int AdminAPI::createLesson(const std::unordered_multimap<std::string, std::strin
     return 200;
 }
 
-int AdminAPI::deleteLesson(const int lesson_id) {
-    Lesson::remove(lesson_id);
+int AdminAPI::updateLesson(const std::unordered_multimap<std::string, std::string> &data, const User &user) {
+    if (data.empty()) {
+        return 404;
+    }
+
+    int lesson_id = std::stoi(get("lesson_id", data));
+    int course_id = std::stoi(get("course", data));
+    std::vector<int> students_id;
+    for (auto &pair : data) {
+        if (pair.first == "new_student") {
+            Lesson::add_student(std::stoi(pair.second), lesson_id, course_id);
+        } else if (pair.first == "delete_student") {
+            Lesson::delete_student(std::stoi(pair.second), lesson_id, course_id);
+        }
+    }
+
+    auto lesson = Lesson::get_lesson(lesson_id);
+    lesson.update(lesson_id,
+                  std::stoi(get("course", data)),
+                  std::stoi(get("cabinet", data)),
+                  std::stoi(get("teacher", data)),
+                  std::stoi(get("weekday", data)),
+                  get("start-hours", data) + ":" + get("start-minutes", data),
+                  get("end-hours", data) + ":" + get("end-minutes", data),
+                  lesson.school_id());
+    auto students = lesson.get_students();
+
     return 200;
 }
 
-std::string AdminAPI::schedule(const User &user) {
-    auto context = mainScheduleStaffData(user);
-    context.putArray("teachers", user.get_teachers(), PersonSerializer<Teacher>());
-    context.putArray("courses", user.get_courses(), CourseSerializer);
-    context.putArray("cabinets", user.get_cabinets(), SimpleTitleSerializer<Cabinet>());
-    _render.set("scheduleStaff.html");
-    return _render.render(context);
+int AdminAPI::deleteLesson(int lesson_id) {
+    Lesson::remove(lesson_id);
+
+    return 200;
+}
+
+int AdminAPI::saveCurrentLesson(const std::unordered_multimap<std::string, std::string> &data) {
+    if (data.empty()) {
+        return 400;
+    }
+
+    int lesson_id = std::stoi(get("lesson_id", data));
+
+    for (auto &pair : data) {
+        if (pair.first != "check" && pair.first != "lesson_id") {
+            Visit::save(std::stoi(pair.first), lesson_id, pair.second[0] == '1');
+        }
+    }
+
+    return 200;
+}
+
+std::pair<int, templates::Context>
+AdminAPI::saveStudent(const std::unordered_multimap<std::string, std::string> &student, const User &user) {
+    templates::Context context;
+
+    if (student.empty()) {
+        return {404, context};
+    }
+
+    std::string result;
+    bool success;
+    std::tie(result, success) = fetch("name", student);
+    if (!success)
+        return {404, context};
+    auto name = result;
+    std::tie(result, success) = fetch("surname", student);
+    if (!success)
+        return {404, context};
+    auto surname = result;
+    auto res = get("age", student);
+    int age = res.empty() ? 0 : std::stoi(res);
+    auto description = get("description", student);
+    auto tel_number = get("tel_number", student);
+    auto email = get("email", student);
+    auto parent = get("parent", student);
+    auto avatar = get("avatar", student);
+
+    int school_id = user.school_id();
+
+    int id = Student::save(name, surname, age, description, tel_number, email, parent, avatar, school_id);
+    context.put("id", id);
+
+    return {200, context};
+}
+
+int AdminAPI::deleteStudents(const std::unordered_multimap<std::string, std::string> &students, const User &user) {
+    for (auto &pair : students) {
+        if (pair.first == "id")
+            Student::remove(std::stoi(pair.second));
+    }
+
+    return 200;
+}
+
+templates::Context AdminAPI::searchStudent(const std::string &search, const User &user) {
+    templates::Context context;
+    auto students = Student::get_students_like(search, user.school_id());
+    context.putArray("students", students, SimplePersonSerializer<Student>());
+
+    return context;
+}
+
+templates::Context AdminAPI::verboseSearchStudent(const std::string &search, const User &user) {
+    templates::Context context;
+    auto students = Student::get_students_like(search, user.school_id());
+    context.putArray("students", students, StudentDBSerializer);
+
+    return context;
+}
+
+int AdminAPI::editCourse(const std::unordered_multimap<std::string, std::string> &data, const User &user) {
+    if (data.empty()) {
+        return 404;
+    }
+
+    std::string result;
+    bool success;
+    std::tie(result, success) = fetch("title", data);
+    if (!success)
+        return 404;
+    auto name = result;
+
+    std::tie(result, success) = fetch("price", data);
+    if (!success)
+        return 404;
+    int price = std::stoi(result);
+
+    std::tie(result, success) = fetch("id", data);
+    if (!success)
+        return 404;
+    int id = std::stoi(result);
+
+    if (id == -1) {
+        Course::save(name, price, user.school_id());
+    } else {
+        Course::update(id, name, price, user.school_id());
+    }
+
+    return 200;
+}
+
+int AdminAPI::deleteCourse(int courseId) {
+    try {
+        Course::remove(courseId);
+        return 200;
+    } catch(...) {
+        return 400;
+    }
 }
 
 int AdminAPI::editTeacher(const std::unordered_multimap<std::string, std::string> &data, const User &user) {
@@ -314,17 +297,46 @@ int AdminAPI::deleteCabinet(int cabinet_id) {
     return 200;
 }
 
-templates::Context VisitSerializer(const Visit& visit) {
+std::string AdminAPI::schedule(const User &user) {
+    auto context = mainScheduleStaffData(user);
+    context.putArray("teachers", user.get_teachers(), PersonSerializer<Teacher>());
+    context.putArray("courses", user.get_courses(), CourseSerializer);
+    context.putArray("cabinets", user.get_cabinets(), SimpleTitleSerializer<Cabinet>());
+    _render.set("scheduleStaff.html");
+    return _render.render(context);
+}
+
+std::string AdminAPI::getPagePaymentsByStudent(const std::string &) {
+    return std::string();
+}
+
+templates::Context AdminAPI::StudentDBSerializer(const Student &student) {
+    templates::Context context;
+    context.put("name", student.name() + " " + student.surname());
+    context.put("avatar", "");
+    context.put("id", student.id());
+    context.put("age", student.age());
+    std::vector<std::string> courses;
+    for (auto &course : student.get_courses()) {
+        courses.push_back(course.title());
+    }
+    context.putArray("courses", courses);
+
+    return context;
+}
+
+templates::Context AdminAPI::VisitSerializer(const Visit& visit) {
     templates::Context context;
     auto lesson = visit.get_lesson();
     context.put("wasInClass", visit.was_in_class());
     context.put("course", lesson.get_course().title());
     context.put("weekday", DateTime::weekdayToStr(lesson.weekday()));
     context.put("date", visit.date());
+
     return context;
 }
 
-templates::Context StudentVerboseSerializer(const Student& student) {
+templates::Context AdminAPI::StudentVerboseSerializer(const Student& student) {
     templates::Context context;
     context.put("id", student.id());
     context.put("name", student.name());
@@ -336,14 +348,8 @@ templates::Context StudentVerboseSerializer(const Student& student) {
     context.put("email", student.email());
     context.put("description", student.description());
     context.putArray("visits", student.get_visits(), VisitSerializer);
+
     return context;
 }
 
-std::string AdminAPI::getStudentPage(int student_id, const User& user) {
-    templates::Context context;
-    auto student = Student::get_student(student_id);
-    context.set("student", StudentVerboseSerializer(student));
-    context.set("user", UserSerializer(user));
-    _render.set("student.html");
-    return _render.render(context);
-}
+
